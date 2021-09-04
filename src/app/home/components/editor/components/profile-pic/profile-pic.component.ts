11beee6186ea16272ac628d7e3506f5b6146fe85
Dinesh Srinivasan <dinesh.srinivasan@nielsen.com>
2021-09-04 12:25:48 -0300

    Potential Fix for picture upload

--- src/app/home/components/editor/components/profile-pic/profile-pic.component.ts
+++ src/app/home/components/editor/components/profile-pic/profile-pic.component.ts
@@ -15,11 +15,15 @@ export class ProfilePicComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  onSelectFile(event: any): void {
+  async onSelectFile(event: any): Promise<void> {
+    await this.uploadImage(event);
+  }
+
+  uploadImage(event: any): void {
     const reader = new FileReader();
     reader.readAsDataURL(event.target.files[0]); // read file as data url
     reader.onload = (event) => { // called once readAsDataURL is completed
-      if(event.target?.result &&  typeof event.target?.result == 'string'){
+      if (event.target?.result && typeof event.target?.result == 'string') {
         this.url = event.target?.result || "";
       } else {
         console.error(`Invalid Image File: ${event.target?.result}`);
